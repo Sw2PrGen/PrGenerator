0faa859114bfb6a429a2f2ad1c5980d53a3b72fa
rusinda <>
2013-04-02 13:32:28 +0200

    initiale Erstellung

---
+++ PrGenerator/src/prgenerator/PrGenerator.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prgenerator;
+
+/**
+ *
+ * @author rusinda
+ */
+public class PrGenerator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
