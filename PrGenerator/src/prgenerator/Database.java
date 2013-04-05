cf544755c4cba1ab0c2fe2e26bec509115c7e6b8
Katharina Sandrock <katharina_sandrock@yahoo.de>
2013-04-05 22:06:43 +0200

    added GUI added setter/ getter to the database

--- PrGenerator/src/prgenerator/Database.java
+++ PrGenerator/src/prgenerator/Database.java
@@ -45,5 +45,173 @@ public class Database {
     public void manageData(){
         
     }
+
+    /**
+     * @return the currentData
+     */
+    public LinkedList<String> getCurrentData() {
+        return currentData;
+    }
+
+    /**
+     * @return the userInput
+     */
+    public String getUserInput() {
+        return userInput;
+    }
+
+    /**
+     * @param userInput the userInput to set
+     */
+    public void setUserInput(String userInput) {
+        this.userInput = userInput;
+    }
+
+    /**
+     * @return the createdText
+     */
+    public String getCreatedText() {
+        return createdText;
+    }
+
+    /**
+     * @param createdText the createdText to set
+     */
+    public void setCreatedText(String createdText) {
+        this.createdText = createdText;
+    }
+
+    /**
+     * @return the createdAbstract
+     */
+    public String getCreatedAbstract() {
+        return createdAbstract;
+    }
+
+    /**
+     * @param createdAbstract the createdAbstract to set
+     */
+    public void setCreatedAbstract(String createdAbstract) {
+        this.createdAbstract = createdAbstract;
+    }
+
+    /**
+     * @return the createdHeading
+     */
+    public String getCreatedHeading() {
+        return createdHeading;
+    }
+
+    /**
+     * @param createdHeading the createdHeading to set
+     */
+    public void setCreatedHeading(String createdHeading) {
+        this.createdHeading = createdHeading;
+    }
+
+    /**
+     * @return the templatesAbstract
+     */
+    public LinkedList<String> getTemplatesAbstract() {
+        return templatesAbstract;
+    }
+
+    /**
+     * @param templatesAbstract the templatesAbstract to set
+     */
+    public void setTemplatesAbstract(LinkedList<String> templatesAbstract) {
+        this.templatesAbstract = templatesAbstract;
+    }
+
+    /**
+     * @return the templatesHeading
+     */
+    public LinkedList<String> getTemplatesHeading() {
+        return templatesHeading;
+    }
+
+    /**
+     * @param templatesHeading the templatesHeading to set
+     */
+    public void setTemplatesHeading(LinkedList<String> templatesHeading) {
+        this.templatesHeading = templatesHeading;
+    }
+
+    /**
+     * @return the finalDocument
+     */
+    public String getFinalDocument() {
+        return finalDocument;
+    }
+
+    /**
+     * @param finalDocument the finalDocument to set
+     */
+    public void setFinalDocument(String finalDocument) {
+        this.finalDocument = finalDocument;
+    }
+
+    /**
+     * @return the finalHtmlDokument
+     */
+    public String getFinalHtmlDokument() {
+        return finalHtmlDokument;
+    }
+
+    /**
+     * @param finalHtmlDokument the finalHtmlDokument to set
+     */
+    public void setFinalHtmlDokument(String finalHtmlDokument) {
+        this.finalHtmlDokument = finalHtmlDokument;
+    }
+
+    /**
+     * @return the pictureList
+     */
+    public LinkedList<String> getPictureList() {
+        return pictureList;
+    }
+
+    /**
+     * @return the userInputFiltered
+     */
+    public LinkedList<String> getUserInputFiltered() {
+        return userInputFiltered;
+    }
+
+    /**
+     * @param userInputFiltered the userInputFiltered to set
+     */
+    public void setUserInputFiltered(LinkedList<String> userInputFiltered) {
+        this.userInputFiltered = userInputFiltered;
+    }
+
+    /**
+     * @return the templateFill
+     */
+    public String[] getTemplateFill() {
+        return templateFill;
+    }
+
+    /**
+     * @param templateFill the templateFill to set
+     */
+    public void setTemplateFill(String[] templateFill) {
+        this.templateFill = templateFill;
+    }
+
+    /**
+     * @return the chosenPicture
+     */
+    public String getChosenPicture() {
+        return chosenPicture;
+    }
+
+    /**
+     * @param chosenPicture the chosenPicture to set
+     */
+    public void setChosenPicture(String chosenPicture) {
+        this.chosenPicture = chosenPicture;
+    }
     
 }
--- PrGenerator/src/prgenerator/Gui.java
+++ PrGenerator/src/prgenerator/Gui.java
@@ -14,63 +14,60 @@ import javax.swing.*;
  * @author Dawid Rusin, Katharina Sandrock
  */
 public class Gui extends JFrame {
-    
-        private JLabel backgroundPicture = new JLabel(new ImageIcon("C:/Users/sandrock/DHBW/4. Semester/Software Eng/GUI/GUI_backgroundpicture.png"));
-        private JTextField userInput = new JTextField(); 
-        private JButton generateTextButton = new JButton();
-    
-        public Gui(){
-        
+
+    private JLabel backgroundPicture = new JLabel(new ImageIcon("C:/Users/sandrock/DHBW/4. Semester/Software Eng/GUI/GUI_backgroundpicture.png"));
+    private JTextField userInput = new JTextField();
+    private JButton generateTextButton = new JButton();
+
+    public Gui() {
+
         setLayout(null);
-        setPreferredSize(new Dimension(465, 380));
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        
+
         backgroundPicture.setBounds(0, 0, 465, 350);
-        
-        userInput.setBounds(206,79,144,26);
-                
-        generateTextButton.setBounds(240,135,77,26);
-        generateTextButton.setBackground(new Color(181,57,24));
+
+        userInput.setBounds(206, 79, 144, 26);
+        userInput.setText("Mögliche Suchbegriffe hier eingeben...");
+
+        generateTextButton.setBounds(240, 135, 77, 26);
+        generateTextButton.setBackground(new Color(181, 57, 24));
         generateTextButton.setText("Start");
         generateTextButton.setForeground(Color.white);
-        add(generateTextButton);
-       
-        
-        generateTextButton.addActionListener(new java.awt.event.ActionListener() {			
-        public void actionPerformed(java.awt.event.ActionEvent evt) {				
-                setInput(evt);			
-            }
 
-        private void setInput(ActionEvent evt) {
-                
-      
-                /* 
-                 * 
-                 * Database data = new Database();
-                 * data.setUserInput(userInput.getText()); 
-                 * 
-                 */
-                 
+
+        generateTextButton.addActionListener(new java.awt.event.ActionListener() {
+
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                setInput(evt);
             }
         });
-        
-           
-        add(backgroundPicture);
-        add(userInput);
-        add(generateTextButton); 
-        
-        setLocation(50, 50);		
-       	setLocationRelativeTo(null);
-        pack();
-        setResizable(false);
 
+
+
+        add(userInput);
+        add(generateTextButton);
+        add(backgroundPicture);
+
+        setResizable(false);
+        setPreferredSize(new Dimension(465, 380));
+        setLocation(50, 50);
+        pack();
         setVisible(true);
+
     }
-    
+
+    private void setInput(ActionEvent evt) {
+        /*
+         *
+         * Database data = new Database();
+         * data.setUserInput(userInput.getText());
+         *
+         */
+    }
+
     public void showResult() {
-        
     }
-    
+
     public boolean saveResult() {
         return true;
     }
