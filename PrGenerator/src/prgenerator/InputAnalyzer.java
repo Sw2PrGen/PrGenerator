334e0cf722502f1998be78951b8d4e89edd65fbf
Stefan <Stefan@141.72.112.67>
2013-04-11 19:11:50 +0200

    Input Analyzer läuft, nur andbindung, bzw Aufruf fehlt noch

--- PrGenerator/src/prgenerator/InputAnalyzer.java
+++ PrGenerator/src/prgenerator/InputAnalyzer.java
@@ -4,10 +4,105 @@
  */
 package prgenerator;
 
+import java.util.LinkedList;
+
 /**
  *
- * @author rusinda
+ * @author Stefan Dreher
  */
 public class InputAnalyzer {
-    
+
+    private static void modifyInputtoString() {
+        // String rarestring=PrGenerator.mainDatabase.getUserInput(); 
+        //rareString=rareString.replace("und", rareString)
+        String rareString = "Prof. Dr. Stefan    Bauch Steine im Weg Mannheim auch§$%&/\\";
+
+        if(rareString.equals("Mögliche Suchbegriffe hier eingeben...")){
+                      
+        }else{
+        
+        //Deletion of abreviations
+        rareString = rareString.replaceAll("allg\\.", "");
+        rareString = rareString.replaceAll("bzw\\.", "");
+        rareString = rareString.replaceAll("bspw\\.", "");
+        rareString = rareString.replaceAll("d\\.h\\.", "");
+        rareString = rareString.replaceAll("etc\\.", "");
+        rareString = rareString.replaceAll("usw\\.", "");
+        rareString = rareString.replaceAll("geb\\.", "");
+        rareString = rareString.replaceAll("s\\.", "");
+        rareString = rareString.replaceAll("od\\.", "");
+        rareString = rareString.replaceAll("s\\.a\\.", "");
+        rareString = rareString.replaceAll("u\\.", "");
+        rareString = rareString.replaceAll("u\\.a\\.", "");
+        rareString = rareString.replaceAll("u\\.Ä\\.", "");
+        rareString = rareString.replaceAll("v\\.a\\.", "");
+        rareString = rareString.replaceAll("vgl\\.", "");
+        rareString = rareString.replaceAll("Prof\\.", "");
+        rareString = rareString.replaceAll("Dr\\.", "");
+        rareString = rareString.replaceAll("Dpl\\.", "");
+        rareString = rareString.replaceAll("Mr\\.", "");
+        rareString = rareString.replaceAll("Ms\\.", "");
+        rareString = rareString.replaceAll("Anh\\.", "");
+        rareString = rareString.replaceAll("Bd\\.", "");
+        rareString = rareString.replaceAll("dgl\\.", "");
+        rareString = rareString.replaceAll("d\\.h\\.", "");
+        rareString = rareString.replaceAll("vlt\\.", "");
+        rareString = rareString.replaceAll("gem\\.", "");
+        rareString = rareString.replaceAll("Nr\\.", "");
+        rareString = rareString.replaceAll("zz\\.", "");
+
+
+
+
+
+        //Deletion of unnecessary words      
+        rareString = rareString.replaceAll("(?i)\\bund\\b", " ");
+        rareString = rareString.replaceAll("(?i)\\baber\\b", " ");
+        rareString = rareString.replaceAll("(?i)\\boder\\b", " ");
+        rareString = rareString.replaceAll("(?i)\\bmit\\b", " ");
+        rareString = rareString.replaceAll("(?i)\\bein\\b", " ");
+        rareString = rareString.replaceAll("(?i)\\beine\\b", " ");
+        rareString = rareString.replaceAll("(?i)\\bohne\\b", " ");
+        rareString = rareString.replaceAll("(?i)\\bwas\\b", " ");
+        rareString = rareString.replaceAll("(?i)\\bauf\\b", " ");
+        rareString = rareString.replaceAll("(?i)\\bunter\\b", " ");
+        rareString = rareString.replaceAll("(?i)\\bzwischen\\b", " ");
+        rareString = rareString.replaceAll("(?i)\\bin\\b", " ");
+        rareString = rareString.replaceAll("(?i)\\bauch\\b", " ");
+        rareString = rareString.replaceAll("(?i)\\bim\\b", " ");
+        rareString = rareString.replaceAll("(?i)\\bweil\\b", " ");
+        rareString = rareString.replaceAll("(?i)\\bwegen\\b", " ");
+
+        //Deletion of special characters
+        rareString = rareString.replaceAll("[^(a-zA-Z)|\\s]", "");
+        
+        //Deletion of unecessary spaces
+        rareString = rareString.replaceAll("\\s\\s+", " ");
+
+//        PrGenerator.mainDatabase.setUserInput(rareString);
+        inputStrToList(rareString);
+        }
+    }
+
+    /**
+     * Funtion 
+     * @param cleanString clean String(user input String without any special caracters, abreviation, fillwords and unnecessary spaces
+     */
+    private static void inputStrToList(String cleanString) {
+        // String cleanString;
+        // cleanString=PrGenerator.mainDatabase.getUserInput();
+        String[] cleanStringArray;
+        cleanStringArray = cleanString.split(" ");
+        LinkedList finalInputList = new <String>LinkedList();
+
+        for (int i = 0; i < cleanStringArray.length; i++) {
+            finalInputList.addFirst(cleanStringArray[i]);
+        }
+        System.out.println(finalInputList);
+    }
+
+    public static void main(String[] args) {
+
+        modifyInputtoString();
+    }
 }
