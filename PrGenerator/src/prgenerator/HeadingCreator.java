aeafa7ddb4a735b42e2771062b524701d7009036
MrWoditschka <jwoditschka@googlemail.com>
2013-04-14 21:44:35 +0200

    added heading creator

--- PrGenerator/src/prgenerator/HeadingCreator.java
+++ PrGenerator/src/prgenerator/HeadingCreator.java
@@ -6,8 +6,34 @@ package prgenerator;
 
 /**
  *
- * @author rusinda
+ * @author Jörg Woditschka
  */
 public class HeadingCreator {
     
+    public void createHeading(){
+        //String[] templateFill= PrGenerator.mainDatabase.getTemplateFill(); //gets filling for the template out of the database
+        String[] templateFill=new String[]{"Biergarten", "heute", "Wettkampf"}; //only for testing 
+        String path="src/sources/templates_heading.xml";
+        String heading = "";
+        Template templateReader = new Template();
+        
+        if(templateFill[0] != null){
+            heading += templateReader.readXML(path, "template");
+            heading = heading.replace("_location_", templateFill[0]);
+        }
+        if(templateFill[1] != null){
+            heading = heading.replace("_date_", templateFill[1]);
+        }
+        if(templateFill[2] != null){
+            heading = heading.replace("_keyAspect_", templateFill[2]);
+        }
+        
+        //PrGenerator.mainDatabase.setCreatedHeading(heading); //puts heading into the database
+        System.out.println("Überschrift \""+heading+"\" in Database abgelegt.");
+    }
+    
+    public static void main(String args[]){ //only for testing
+        HeadingCreator heading = new HeadingCreator();
+        heading.createHeading();
+    }
 }
