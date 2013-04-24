e1b197de3d35c12f37cb76fca9fc9b41a96cb8cd
rusinda <>
2013-04-24 11:00:54 +0200

    Comments + JavaDoc

--- PrGenerator/src/prgenerator/PrGenerator.java
+++ PrGenerator/src/prgenerator/PrGenerator.java
@@ -4,18 +4,16 @@
  */
 package prgenerator;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JOptionPane;
-import javax.swing.text.html.Option;
 
 /**
+ * Main Class of the project, holds static instances of all classes needed and
+ * manages the control flow of the application
  *
- * @author rusinda
+ * @author Dawid Rusin
  */
 public class PrGenerator {
-    
-//    public static Gui mainGui;
+
     public static Thread mainGui = new Thread(new Gui());
     public static AbstractCreator mainAbstractCreator;
     public static Database mainDatabase;
@@ -29,44 +27,47 @@ public class PrGenerator {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        mainDatabase = new Database();
+        mainDatabase = new Database();              //create all objects required
         mainAbstractCreator = new AbstractCreator();
         mainHeadingCreator = new HeadingCreator();
         mainInputAnalyzer = new InputAnalyzer();
         mainOutputGenerator = new OutputGenerator();
         mainPictureChooser = new PictureChooser();
         mainTextCreator = new TextCreator();
-  
-        doit();
-    }
-    
-    public static void doit(){
-        mainGui.start();
-        do {
-        while (mainDatabase.getUserInput() == null) {
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException ex) {
-                JOptionPane.showConfirmDialog(null, "Kritischer Fehler! Bitte Programm neustarten.", "Kritischer Fehler", JOptionPane.OK_CANCEL_OPTION, JOptionPane.ERROR_MESSAGE);
-            }
-        }
-        PrGenerator.mainInputAnalyzer.modifyInputToString();
-        PrGenerator.mainDatabase.manageData();
-        PrGenerator.mainTextCreator.createMainText();
-        PrGenerator.mainAbstractCreator.createAbstract();
-        PrGenerator.mainHeadingCreator.createHeading();
-        PrGenerator.mainPictureChooser.choosePicture();
-        PrGenerator.mainOutputGenerator.generateOutput();
-        while (mainDatabase.getUserInput() != null) {
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException ex) {
-                JOptionPane.showConfirmDialog(null, "Kritischer Fehler! Bitte Programm neustarten.", "Kritischer Fehler", JOptionPane.OK_CANCEL_OPTION, JOptionPane.ERROR_MESSAGE);
-            }
-        }
-        } while (true);
-       // mainGui.showResult();
-        
+
+        doit();         //start the main loop
     }
 
+    /**
+     * Main loop of the application, steers the control flow
+     */
+    public static void doit() {
+        mainGui.start();            //GUI is needed as a thread to enable better user experience
+        do {
+            while (mainDatabase.getUserInput() == null) {       //wait fot the userInput to be set
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException ex) {             //something, somewhere went horribly wrong
+                    JOptionPane.showConfirmDialog(null, "Kritischer Fehler! Bitte Programm neustarten.", "Kritischer Fehler", JOptionPane.OK_CANCEL_OPTION, JOptionPane.ERROR_MESSAGE);
+                }
+            }
+            
+            //main control flow
+            PrGenerator.mainInputAnalyzer.modifyInputToString();
+            PrGenerator.mainDatabase.manageData();
+            PrGenerator.mainTextCreator.createMainText();
+            PrGenerator.mainAbstractCreator.createAbstract();
+            PrGenerator.mainHeadingCreator.createHeading();
+            PrGenerator.mainPictureChooser.choosePicture();
+            PrGenerator.mainOutputGenerator.generateOutput();
+            while (mainDatabase.getUserInput() != null) {       //on this point userInput will be reset by pressing "zurück" or the programm will be closed
+                try {
+                    Thread.sleep(100);      //wait for the userinput to be reset to start over again
+                } catch (InterruptedException ex) {             //something, somewhere went horribly wrong
+                    JOptionPane.showConfirmDialog(null, "Kritischer Fehler! Bitte Programm neustarten.", "Kritischer Fehler", JOptionPane.OK_CANCEL_OPTION, JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        } while (true);
+
+    }
 }
